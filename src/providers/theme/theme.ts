be910a19d0ba04af90ef9dd518f9c2be2ac6b0d8
William G <william.godoy53@gmail.com>
2020-06-01 10:06:50 -0300

    refactor(global-style): export GlobalStyle cmp

---
+++ src/providers/theme/theme.ts
@@ -0,0 +1,32 @@
+import { TTheme } from './types';
+
+const colors = {
+  configuration: '#39b2c2',
+  greyBackground: '#F8F8F9',
+  greyText: '#404040',
+  negative: '#cd3636',
+  positive: '#A4FF0E',
+  yellowBackground: '#F9E39E',
+};
+
+const numberFromZeroToHundred = [...Array(21)].map((_, idx) => idx * 5);
+
+const theme: TTheme = {
+  colors,
+  boxShadow: '0 1px 4px -1px #808080',
+  spaces: numberFromZeroToHundred,
+  texts: {
+    normal: {
+      color: colors.greyText,
+      fontSize: '18px',
+      fontWeight: 'normal',
+    },
+    title: {
+      color: colors.greyText,
+      fontSize: '20px',
+      fontWeight: 'bold',
+    },
+  },
+};
+
+export default theme;
